e359a7e5b108df9fe366b09d86fedf5a22b211fa
Rytis Bankieta <rytis@LT-LIT-SC-0166.internal.visma.com>
2020-02-03 15:46:34 +0200

    Move full annotation path to import

--- src/main/java/lt/visma/starter/controller/BasicController.java
+++ src/main/java/lt/visma/starter/controller/BasicController.java
@@ -4,8 +4,9 @@ import lt.visma.starter.service.BasicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping(value = "/sample")
 public class BasicController {
     private BasicService basicService;
