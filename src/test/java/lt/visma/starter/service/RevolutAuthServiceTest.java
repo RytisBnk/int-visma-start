54c16cb0f72830210eace9d740167e7df5cfd7c2
Rytis Bankieta <rytis.bankieta@gmail.com>
2020-02-15 23:19:47 +0200

    Extract string values to constant variables in RevolutAuthServiceTest

--- src/test/java/lt/visma/starter/service/RevolutAuthServiceTest.java
+++ src/test/java/lt/visma/starter/service/RevolutAuthServiceTest.java
@@ -23,28 +23,38 @@ public class RevolutAuthServiceTest {
     private HttpRequestService httpRequestService = new HttpRequestServiceImpl();
     @Mock
     private RevolutConfigurationProperties configurationProperties;
-
     private RovolutAuthenticationService revolutAuthenticationService;
+
+    private final String TEST_EXPECTED_JWT_VALUE = "eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczovL2V4YW1wbGUuY29tIiwic3ViIjoiQUFBQSIsImlzcyI6InRlc3QuY29tIn0.jrOKOEzK4F4YXR8AWfEpu2Rl0N9NNDeBfZ3WhsXzImo-c6Muau_9BAnHw7gsyAUd4pmB2_5TIk0dtqgQZlTLbDs5QxtHIrhYp2Ja9QpKlML6v0-I3UZG_Kka7uczdEeV3wNn3kMW_VIz4WwxFCHRdvCrlIZ8x8uFNlCFyEp9M70";
+    private final String SANDBOX_JWT_TOKEN = "eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczovL3Jldm9sdXQuY29tIiwic3ViIjoiaXpycXkwNzZ0VE9mSnQwQWJMZEtVcWMyMzF4bE1VYmU1TU5iZE85bFJ3TSIsImlzcyI6InJldm9sdXQtand0LXNhbmRib3guZ2xpdGNoLm1lIn0.n7n-VkgTXR1NHWg-AVuZdnlztCYIvfyWo5_RtcqgBTkl7ur17Ys5wlyZ8n9d9SJKSWZ_5ONLUrY4uUW9jH_S7_MT-YQYoia5rfmnLOMW1TZGNzMlxRvZ1Usi_oFbFbsppOi2ewimNSUEdCRW5UIilpV9zkZn7yxyVa0Vf3HwQnM";
+    private final String JWT_TEST_AUD = "https://example.com";
+    private final String JWT_TEST_ISS = "test.com";
+    private final String JWT_TEST_CLIENTID = "AAAA";
+    private final String PRIVATE_KEY_FILEPATH = "./private_key.der";
+
+    private final String CLIENT_ID = "izrqy076tTOfJt0AbLdKUqc231xlMUbe5MNbdO9lRwM";
+    private final String CLIENT_ASSERTION_TYPE = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+    private final String REFRESH_TOKEN = "oa_sand_iPcIWTe-OgYbOj-p48RN1Y7uGUrlc3y3c-3LIg0eRYs";
+    private final String API_URL = "https://sandbox-b2b.revolut.com/api/1.0";
 
     @Test
     public void testJwtValidity() throws GenericException {
-        when(configurationProperties.getAud()).thenReturn("https://example.com");
-        when(configurationProperties.getIss()).thenReturn("test.com");
-        when(configurationProperties.getClientId()).thenReturn("AAAA");
-        when(configurationProperties.getPrivateKeyFilepath()).thenReturn("./private_key.der");
+        when(configurationProperties.getAud()).thenReturn(JWT_TEST_AUD);
+        when(configurationProperties.getIss()).thenReturn(JWT_TEST_ISS);
+        when(configurationProperties.getClientId()).thenReturn(JWT_TEST_CLIENTID);
+        when(configurationProperties.getPrivateKeyFilepath()).thenReturn(PRIVATE_KEY_FILEPATH);
 
-        assertEquals("eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczovL2V4YW1wbGUuY29tIiwic3ViIjoiQUFBQSIsImlzcyI6InRlc3QuY29tIn0.jrOKOEzK4F4YXR8AWfEpu2Rl0N9NNDeBfZ3WhsXzImo-c6Muau_9BAnHw7gsyAUd4pmB2_5TIk0dtqgQZlTLbDs5QxtHIrhYp2Ja9QpKlML6v0-I3UZG_Kka7uczdEeV3wNn3kMW_VIz4WwxFCHRdvCrlIZ8x8uFNlCFyEp9M70",
-                revolutAuthenticationService.getJWTToken());
+        assertEquals(TEST_EXPECTED_JWT_VALUE, revolutAuthenticationService.getJWTToken());
     }
 
     @Test
     public void testAccessTokenRefreshing() throws GenericException, ApiException {
-        when(configurationProperties.getClientId()).thenReturn("izrqy076tTOfJt0AbLdKUqc231xlMUbe5MNbdO9lRwM");
-        when(configurationProperties.getClientAssertionType()).thenReturn("urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
-        when(configurationProperties.getRefreshToken()).thenReturn("oa_sand_iPcIWTe-OgYbOj-p48RN1Y7uGUrlc3y3c-3LIg0eRYs");
-        when(configurationProperties.getApiURL()).thenReturn("https://sandbox-b2b.revolut.com/api/1.0");
+        when(configurationProperties.getClientId()).thenReturn(CLIENT_ID);
+        when(configurationProperties.getClientAssertionType()).thenReturn(CLIENT_ASSERTION_TYPE);
+        when(configurationProperties.getRefreshToken()).thenReturn(REFRESH_TOKEN);
+        when(configurationProperties.getApiURL()).thenReturn(API_URL);
 
-        RevolutAccessToken accessToken = revolutAuthenticationService.refreshAccessToken("eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJodHRwczovL3Jldm9sdXQuY29tIiwic3ViIjoiaXpycXkwNzZ0VE9mSnQwQWJMZEtVcWMyMzF4bE1VYmU1TU5iZE85bFJ3TSIsImlzcyI6InJldm9sdXQtand0LXNhbmRib3guZ2xpdGNoLm1lIn0.n7n-VkgTXR1NHWg-AVuZdnlztCYIvfyWo5_RtcqgBTkl7ur17Ys5wlyZ8n9d9SJKSWZ_5ONLUrY4uUW9jH_S7_MT-YQYoia5rfmnLOMW1TZGNzMlxRvZ1Usi_oFbFbsppOi2ewimNSUEdCRW5UIilpV9zkZn7yxyVa0Vf3HwQnM");
+        RevolutAccessToken accessToken = revolutAuthenticationService.refreshAccessToken(SANDBOX_JWT_TOKEN);
         assertNotNull(accessToken);
         assertNotNull(accessToken.getAccessToken());
         assertNotNull(accessToken.getExpiresIn());
