bfbae02886b98408c59ba695c8b1ba32193a2342
RytisBnk <rytis.bankieta@gmail.com>
2020-02-20 17:09:31 +0200

    Remove unused exception class

--- src/main/java/lt/visma/starter/exception/ParameterNotProvidedException.java
+++
@@ -1,8 +0,0 @@
-package lt.visma.starter.exception;
-
-public class ParameterNotProvidedException extends Exception{
-
-    public ParameterNotProvidedException(String message) {
-        super(message);
-    }
-}
