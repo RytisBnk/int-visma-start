d1cc7013d8cc9075c1b409349b6cb2b98a729c87
RytisBnk <rytis.bankieta@gmail.com>
2020-02-21 15:51:53 +0200

    Fix repository primary key specifications

--- src/main/java/lt/visma/starter/repository/RevolutPaymentRepository.java
+++ src/main/java/lt/visma/starter/repository/RevolutPaymentRepository.java
@@ -2,11 +2,6 @@ package lt.visma.starter.repository;
 
 import lt.visma.starter.model.revolut.entity.RevolutTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
-public interface RevolutPaymentRepository extends JpaRepository<RevolutTransaction, Long> {
-    @Query(value = "select t from RevolutTransaction t where t.id = ?1")
-    Optional<RevolutTransaction> findById(String id);
+public interface RevolutPaymentRepository extends JpaRepository<RevolutTransaction, String> {
 }
--- src/main/java/lt/visma/starter/repository/SwedbankPaymentRepositotory.java
+++ src/main/java/lt/visma/starter/repository/SwedbankPaymentRepositotory.java
@@ -2,13 +2,8 @@ package lt.visma.starter.repository;
 
 import lt.visma.starter.model.swedbank.entity.SwedbankPaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface SwedbankPaymentRepositotory extends JpaRepository<SwedbankPaymentTransaction, Long> {
-    @Query(value = "select t from SwedbankPaymentTransaction t where t.id = ?1")
-    Optional<SwedbankPaymentTransaction> findById(String id);
+public interface SwedbankPaymentRepositotory extends JpaRepository<SwedbankPaymentTransaction, String> {
 }
